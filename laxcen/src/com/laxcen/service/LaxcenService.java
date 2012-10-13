a0f01824bb6810e3fd447b83ca055364a0d2fc56
Dihui Bao <abalone.bdh@gmail.com>
2012-10-13 23:37:08 +0800

    add Spring MVC & add spring project nature

---
+++ laxcen/src/com/laxcen/service/LaxcenService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.laxcen.service;
+
+/**
+ * @author abalone
+ *
+ */
+public class LaxcenService {
+	
+	public String service() {
+		return "laxcen page";
+	}
+}
---
+++ laxcen/src/com/laxcen/web/controller/LaxcenController.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.laxcen.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.laxcen.service.LaxcenService;
+
+/**
+ * @author abalone
+ *
+ */
+public class LaxcenController implements Controller {
+
+	private LaxcenService laxcenService;
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		return new ModelAndView("laxcen","laxcen",laxcenService.service());
+	}
+
+	public void setLaxcenService(LaxcenService laxcenService) {
+		this.laxcenService = laxcenService;
+	}
+	
+}
