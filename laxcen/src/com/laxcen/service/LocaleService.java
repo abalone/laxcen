b580636c1484f9b5fe7592167965c9140723a17a
Dihui Bao <abalone.bdh@gmail.com>
2012-10-17 23:24:26 +0800

    add logback functionality

--- laxcen/src/com/laxcen/service/LocaleService.java
+++ laxcen/src/com/laxcen/service/LocaleService.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.google.common.collect.ImmutableMap;
@@ -17,17 +19,18 @@ import com.google.common.collect.ImmutableMap;
  */
 public class LocaleService {
 	
+	final static Logger logger = LoggerFactory.getLogger(LocaleService.class);
+	
 	private final static ImmutableMap<String,String> siteLanguagePickerMap = ImmutableMap.of("zh_CN","en_US","en_US","zh_CN");
 
 	public String getLocaleString(HttpServletRequest request) {
 		Locale locale = RequestContextUtils.getLocaleResolver(request)
 				.resolveLocale(request);
-		System.out.println(locale.getLanguage() + locale.getCountry());
+		logger.warn("Get Locale String: {}_{}",locale.getLanguage(), locale.getCountry());
 		if (locale != null) {
 			return String.format("?%s=%s", "siteLanguage",
 					siteLanguagePickerMap.get(locale.getLanguage()+"_"+locale.getCountry()));
 		}
 		return "";
 	}
-
 }
