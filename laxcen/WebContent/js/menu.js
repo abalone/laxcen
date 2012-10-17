b41df054dc48e93a5457b8452e18ac7e52930285
Dihui Bao <abalone.bdh@gmail.com>
2012-10-17 23:21:36 +0800

    update javascript

--- laxcen/WebContent/js/menu.js
+++ laxcen/WebContent/js/menu.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+	$(".menubar").find("li").each(function(){
+		$(this).hover(function(){$(this).addClass("menuhighlight");$(this).find("a").addClass("menuhighlight");});
+		$(this).mouseleave(function(){$(this).removeClass("menuhighlight");$(this).find("a").removeClass("menuhighlight");});
+	});
+	
 	$(".submenubar").find("li").each(function(){
 		$(this).hover(function(){$(this).addClass("submenuhighlight");$(this).find("a").addClass("submenuhighlight");});
 		$(this).mouseleave(function(){$(this).removeClass("submenuhighlight");$(this).find("a").removeClass("submenuhighlight");});
 	});
+	
 });
